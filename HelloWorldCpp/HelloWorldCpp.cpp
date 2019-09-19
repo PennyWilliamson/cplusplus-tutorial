b71a42d8f84bd72ce8cde15bb8243273daeea645
PennyWilliamson <catspaw73@outlook.com>
2019-09-19 13:59:19 +1200

    First HelloWorld program.

---
+++ HelloWorldCpp/HelloWorldCpp.cpp
@@ -0,0 +1,10 @@
+// HelloWorldCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World";
+	return 0;
+}
