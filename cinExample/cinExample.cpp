5e79e494c9a89fbd3c0c5895f8b6823694df75c2
PennyWilliamson <catspaw73@outlook.com>
2019-09-20 09:02:57 +1200

    cin example

---
+++ cinExample/cinExample.cpp
@@ -0,0 +1,14 @@
+// cinExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a number\n";
+	int x{};
+	std::cin >> x;
+	std::cout << "You entered " << x << '\n';
+	return 0;
+}
+
